6d9d00d02f222e5b1207927fda45be503b570bb2
Ricardo MONLA <rmonla@gmail.com>
2025-03-24 21:55:09 -0300

    Actualiza los nombres para mostrar de audioFiles

--- incs/audioFiles.php
+++ incs/audioFiles.php
@@ -3,97 +3,97 @@
 $audioFiles = [
 [
     'filename' => '101v25-1_La_Entrada_en_Jerusalen.mp3',
-    'display_name' => '1ºP - La Entrada en Jerusalen',
+    'display_name' => '101 - La entrada de Jesús en Jerusalén',
     'id' => '101v25-1', 'order' => '0102', 'short_url' => '',
 ],
 [
     'filename' => '102v25-1_El_Trato_de_Judas_y_Caifás.mp3',
-    'display_name' => '1ºP - El Trato de Judas y Caifás',
+    'display_name' => '102 - El Trato de Judas y Caifás',
     'id' => '102v25-1', 'order' => '0103', 'short_url' => '',
 ],
 [
     'filename' => '103v25-6_La_Última_Cena+Monedas_Adeje16.mp3',
-    'display_name' => '1ºP - La Última Cena',
+    'display_name' => '103 - La Última Cena',
     'id' => '103v25-6', 'order' => '0104', 'short_url' => '',
 ],
 [
     'filename' => '104v25-1_El_Monte_de_los_olivos.mp3',
-    'display_name' => '1ºP - El Monte de los olivos',
+    'display_name' => '104 - La oración en el Monte de los Olivos',
     'id' => '104v25-1', 'order' => '0105', 'short_url' => '',
 ],
 [
     'filename' => '105v25-1_La_Entrega.mp3',
-    'display_name' => '1ºP - La Entrega',
+    'display_name' => '105 - La Entrega',
     'id' => '105v25-1', 'order' => '0106', 'short_url' => '',
 ],
 [
     'filename' => '106v25-1_La_Negación_de_Pedro.mp3',
-    'display_name' => '1ºP - La Negación de Pedro',
+    'display_name' => '106 - Las negaciones de Pedro',
     'id' => '106v25-1', 'order' => '0107', 'short_url' => '',
 ],
 [
     'filename' => '107v25-1_El_Juicio_en_San_Edrin.mp3',
-    'display_name' => '1ºP - El Juicio en San Edrin',
+    'display_name' => '107 - El juicio en el Sanedrín',
     'id' => '107v25-1', 'order' => '0108', 'short_url' => '',
 ],
 [
     'filename' => '108v25-1_La_Culpa_de_Judas.mp3',
-    'display_name' => '1ºP - La Culpa de Judas',
+    'display_name' => '108 - La Culpa de Judas',
     'id' => '108v25-1', 'order' => '0109', 'short_url' => '',
 ],
 [
     'filename' => '109v25-1_Jesús_Ante_Ponsio_Pilatos.mp3',
-    'display_name' => '1ºP - Jesús Ante Ponsio Pilatos',
+    'display_name' => '109 - El lavado de las manos de Pilatos',
     'id' => '109v25-1', 'order' => '0110', 'short_url' => '',
 ],
 [
     'filename' => '110v25-1_Jesús_Ante_Herodes.mp3',
-    'display_name' => '1ºP - Jesús Ante Herodes',
+    'display_name' => '110 - Jesús Ante Herodes',
     'id' => '110v25-1', 'order' => '0111', 'short_url' => '',
 ],
 [
     'filename' => '111v25-1_1raE_Jesús_es_Condenado_a_Muerte.mp3',
-    'display_name' => '1ºP - 1ra Estación - Jesús es Condenado a Muerte',
+    'display_name' => '111 - 1ºE - Jesús es condenado a muerte',
     'id' => '111v25-1', 'order' => '0112', 'short_url' => '',
 ],
 [
-    'filename' => '199v3_TodoUnido.mp3',
+    'filename' => '199v25-1_TodoUnido.mp3',
     'display_name' => '1ºP - Todo UNIDO',
     'id' => 'VCBY0199', 'order' => '0199', 'short_url' => '',
 ],
 [
     'filename' => '201-E02_Jesús_es_cargado_con_la_cruz.mp3',
-    'display_name' => '2ºP - E02 Jesús es cargado con la cruz',
+    'display_name' => '201 - 2ºE - Jesús carga con la cruz',
     'id' => 'VCBY0201', 'order' => '0201', 'short_url' => '',
 ],
 [
     'filename' => '202-E03_Jesús_cae_por_primera_vez.mp3',
-    'display_name' => '2ºP - E03 Jesús cae por primera vez',
+    'display_name' => '202 - 3ºE - Jesús cae por primera vez',
     'id' => 'VCBY0202', 'order' => '0202', 'short_url' => '',
 ],
 [
     'filename' => '203-E04_Jesús_encuentra_a_su_madre.mp3',
-    'display_name' => '2ºP - E04 Jesús encuentra a su madre',
+    'display_name' => '203 - 4ºE - Jesús se encuentra con su madre',
     'id' => 'VCBY0203', 'order' => '0203', 'short_url' => '',
 ],
 [
     'filename' => '204-E05_Simón_de_Cirene_ayuda_a_Jesús.mp3',
-    'display_name' => '2ºP - E05 Simón de Cirene ayuda a Jesús',
+    'display_name' => '204 - 5ºE - Simón de Cirene ayuda a Jesús a llevar la cruz',
     'id' => 'VCBY0204', 'order' => '0204', 'short_url' => '',
 ],
 [
     'filename' => '205-E06_La_Verónica_enjuga_el_rostro_de_Jesús.mp3',
-    'display_name' => '2ºP - E06 La Verónica enjuga el rostro de Jesús',
+    'display_name' => '205 - 6ºE - La Verónica enjuga el rostro de Jesús',
     'id' => 'VCBY0205', 'order' => '0205', 'short_url' => '',
 ],
 [
     'filename' => '206-E07_Jesús_cae_por_segunda_vez.mp3',
-    'display_name' => '2ºP - E07 Jesús cae por segunda vez',
+    'display_name' => '206 - 7ºE - Jesús cae por segunda vez',
     'id' => 'VCBY0206', 'order' => '0206', 'short_url' => '',
 ],
 [
     'filename' => '207-E08_Jesús_consuela_a_las_santas_mujeres.mp3',
-    'display_name' => '2ºP - E08 Jesús consuela a las santas mujeres',
+    'display_name' => '207 - 8ºE - Jesús consuela a las mujeres de Jerusalén',
     'id' => 'VCBY0207', 'order' => '0207', 'short_url' => '',
 ],
 [
@@ -103,47 +103,47 @@ $audioFiles = [
 ],
 [
     'filename' => '301v2-E09_Jesús_Cae_por_Tercera_Vez.mp3',
-    'display_name' => '3ºP - E09 Jesús Cae por Tercera Vez',
+    'display_name' => '301 - 9ºE - Jesús cae por tercera vez',
     'id' => 'VCBY0301', 'order' => '0301', 'short_url' => '',
 ],
 [
     'filename' => '302v2-E10_Jesús_Despojado_de_sus_Vestiduras.mp3',
-    'display_name' => '3ºP - E10 Jesús Despojado de sus Vestiduras',
+    'display_name' => '302 - 10ºE - Jesús es despojado de sus vestiduras',
     'id' => 'VCBY0302', 'order' => '0302', 'short_url' => '',
 ],
 [
     'filename' => '303v2-E11_Jesús_es_Clavado_en_la_Cruz.mp3',
-    'display_name' => '3ºP - E11 Jesús es Clavado en la Cruz',
+    'display_name' => '303 - 11ºE - Jesús es clavado en la cruz',
     'id' => 'VCBY0303', 'order' => '0303', 'short_url' => '',
 ],
 [
     'filename' => '304v2-E12_Jesús_Muere_en_la_Cruz.mp3',
-    'display_name' => '3ºP - E12 Jesús Muere en la Cruz',
+    'display_name' => '304 - 12ºE - Jesús muere en la cruz',
     'id' => 'VCBY0304', 'order' => '0304', 'short_url' => '',
 ],
 [
     'filename' => '305v2-E13_Jesús_es_Bajado_de_la_Cruz.mp3',
-    'display_name' => '3ºP - E13 Jesús es Bajado de la Cruz',
+    'display_name' => '305 - 13ºE - Jesús es bajado de la cruz y entregado a su madre',
     'id' => 'VCBY0305', 'order' => '0305', 'short_url' => '',
 ],
 [
     'filename' => '306v2-E14_El_Cuerpo_de_Jesús_Depositado_en_el_Sepulcro.mp3',
-    'display_name' => '3ºP - E14 El Cuerpo de Jesús Depositado en el Sepulcro',
+    'display_name' => '306 - 14ºE - Jesús es colocado en el sepulcro',
     'id' => 'VCBY0306', 'order' => '0306', 'short_url' => '',
 ],
 [
     'filename' => '307v2-La_Puerta_de_Piedra.mp3',
-    'display_name' => '3ºP - La Puerta de Piedra',
+    'display_name' => '307 - La Puerta de Piedra',
     'id' => 'VCBY0307', 'order' => '0307', 'short_url' => '',
 ],
 [
     'filename' => '308v2-El_Sepulcro.mp3',
-    'display_name' => '3ºP - El Sepulcro',
+    'display_name' => '308 - El Sepulcro',
     'id' => 'VCBY0308', 'order' => '0308', 'short_url' => '',
 ],
 [
     'filename' => '309v2-El_Anuncio_de_la_Resurección.mp3',
-    'display_name' => '3ºP - El Anuncio de la Resurección',
+    'display_name' => '309 - El Anuncio de la Resurección',
     'id' => 'VCBY0309', 'order' => '0309', 'short_url' => '',
 ]
 ];
